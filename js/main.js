e148ecf0274ab44a9f81e34a94d7b0561cfd89d1
mjimenez1 <mjimenez@anzen.com.mx>
2019-02-27 18:20:35 -0600

    actuales

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ var nameBottles = ["Jumbo", "Big", "Medium", "Small", "Petit"]
 var i=0;
 var faltan = 0;
 var fill = 0;
+var arrayBottles = []
 
 function getLts(lts){
 	numberLts = $(lts).val() //Lee el número de litros a llenar
+	faltan = numberLts;
 	var fullFill = (numberLts/7).toFixed(2) //Solo toma las 2 primeras decimas
 	knowLts(fullFill)
 }
@@ -15,50 +17,95 @@ function getLts(lts){
 
 function knowLts(ltsRest) {
     if (ltsRest % 1 == 0) { //Si es entero 
-    	arrayLts.push(parseInt(ltsRest) + " botella(s) - tamaño  " + nameBottles[i]) //Agrega el numero de botellas que se usaran dependiendo de los litros
-    	printResult() //Lleva al final
-    } else { //Si es decimal
-    	if(ltsRest > 0 && ltsRest < 1){ //Si es mayor a 0 pero menor a 1 el. 0.32
-    		i = i + 1; //Se aumenta el i para recorrer los lts va de mayor a menor
-    		if(faltan == 0){ //Es porque no se ha llenado ningun litro
-    			var newresult = (numberLts/parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
-		       	knowLts(newresult)
-    		}
-    		else{
-    			var newresult = (faltan/parseInt(cantLts[i])).toFixed(2)
-		       	knowLts(newresult)
-    		}
-    	}
-    	else{
-    		var separador = "."; //encuentra el punto en la cadena
-	        var arregloDeSubCadenas = ltsRest.split(separador); //separa las cantidades, enteros y decimales
-	        arrayLts.push(arregloDeSubCadenas[0] + " botella(s) - tamaño  " + nameBottles[i]) //el entero son los lts llenados, las decimales lo que falta y se agrega al arreglo final
-	        var ltsFill = parseInt(arregloDeSubCadenas[0]) * parseInt(cantLts[i]); // se multiplica para saber cuantos litros llenamos
-	        fill = fill + ltsFill; //si ya se tenian llenados antes se suman con los nuevos
-	        faltan = numberLts - fill; //se hace la resta de los faltantes
-	        if(faltan > 0){ //Si aun faltan litros se vuelve a hacer la operación con los litros faltantes
-	        	i = i + 1;
-	        	var newDiv = (faltan/parseInt(cantLts[i])).toFixed(2)
-	        	knowLts(newDiv, i)
-	        }
-    	}
+        if (arrayBottles[i] >= parseInt(ltsRest)) {
+            arrayLts.push(parseInt(ltsRest) + " botella(s) - tamaño  " + nameBottles[i])
+            var ltsFill = cantLts[i] * ltsRest
+            fill = fill + ltsFill
+            faltan = faltan - ltsFill
+            arrayBottles[i] = arrayBottles[i] - parseInt(ltsRest)
+            if (faltan == 0) {
+                printResult()
+            } else {
+                i = i + 1;
+                var newresult = (faltan / parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
+                knowLts(newresult)
+            }
+        } else if (arrayBottles[i] != 0) {
+            arrayLts.push(arrayBottles[i] + " botella(s) - tamaño  " + nameBottles[i])
+            var ltsFill = cantLts[i] * arrayBottles[i];
+            fill = fill + ltsFill;
+            faltan = faltan - ltsFill;
+            arrayBottles[i] = arrayBottles[i] - arrayBottles[i]
+            if (fill == numberLts) {
+                printResult()
+            } else {
+                i = i + 1;
+                var newresult = (faltan / parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
+                knowLts(newresult)
+            }
+        } else {
+            i = i + 1;
+            var newresult = (faltan / parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
+            knowLts(newresult)
+        }
+    } else if(ltsRest == "NaN"){
+    	alert("No hay botellas suficientes")
+    	resetForm()
+    } 
+    else { //Si es decimal
+        if (ltsRest > 0 && ltsRest < 1) { //Si es mayor a 0 pero menor a 1 el. 0.32
+            i = i + 1; //Se aumenta el i para recorrer los lts va de mayor a menor
+            var newresult = (faltan / parseInt(cantLts[i])).toFixed(2)
+            knowLts(newresult)
+        } else {
+            var separador = "."; //encuentra el punto en la cadena
+            var arregloDeSubCadenas = ltsRest.split(separador); //separa las cantidades, enteros y decimales
+            var checkBottles = parseInt(arregloDeSubCadenas[0]) - arrayBottles[i]
+            if (arrayBottles[i] >= parseInt(arregloDeSubCadenas[0])) {
+                arrayLts.push(arregloDeSubCadenas[0] + " botella(s) - tamaño  " + nameBottles[i]) //el entero son los lts llenados, las decimales lo que falta y se agrega al arreglo final
+                var ltsFill = parseInt(arregloDeSubCadenas[0]) * parseInt(cantLts[i]); // se multiplica para saber cuantos litros llenamos
+                fill = fill + ltsFill; //si ya se tenian llenados antes se suman con los nuevos
+                faltan = faltan - ltsFill; //se hace la resta de los faltantes
+                arrayBottles[i] = arrayBottles[i] - parseInt(arregloDeSubCadenas[0])
+                i = i + 1;
+                var newDiv = (faltan / parseInt(cantLts[i])).toFixed(2)
+                knowLts(newDiv, i)
+            } else if (arrayBottles[i] != 0) {
+                arrayLts.push(arrayBottles[i] + " botella(s) - tamaño  " + nameBottles[i])
+                var ltsFill = cantLts[i] * arrayBottles[i];
+                fill = fill + ltsFill;
+                faltan = faltan - ltsFill;
+                arrayBottles[i] = arrayBottles[i] - arrayBottles[i]
+                i = i + 1;
+                var newresult = (faltan / parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
+                knowLts(newresult)
+            } else {
+                i = i + 1;
+                var newresult = (faltan / parseInt(cantLts[i])).toFixed(2) //se hace una nueva division 
+                knowLts(newresult)
+            }
+        }
     }
 }
+
 function printResult(){
 	$("#list-result").empty()
 	for(var j=0; j < arrayLts.length; j++){
 		var appendList = "<li>"+arrayLts[j]+"</li>"
 		$("#list-result").append(appendList)
 	}
-	$("#accept-first").addClass("hidden")
+	$("#accept-first").addClass("d-none")
 	$("#litros").prop("disabled", true)
-	$(".result").removeClass("hidden")
+	$(".result").removeClass("d-none")
+	for(var k=0; k < arrayBottles.length; k++){
+		$(".bottles input:eq("+k+")").val(arrayBottles[k])
+	}
 }
 
 function resetForm(){ 
-	$(".result").addClass("hidden");
+	$(".result").addClass("d-none");
 	$("#litros").prop("disabled", false);
-	$("#accept-first").removeClass("hidden");
+	$("#accept-first").removeClass("d-none");
 	arrayLts = [];
 	$("#litros").val("")
 	i = 0;
@@ -73,4 +120,17 @@ function restrictToNumber(event) {
         event.preventDefault();
     }
     return true
+}
+
+function addBottles(){
+	var jumbo = $("#jumbo").val()
+	var big = $("#big").val()
+	var med = $("#med").val()
+	var small = $("#small").val()
+	var petit = $("#petit").val()
+
+	arrayBottles.push(jumbo, big, med, small, petit)
+	$(".bottles input").prop("disabled", true)
+	$("#btn-add").addClass("d-none")
+	console.log(arrayBottles)
 }
